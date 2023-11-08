0798b6a3ba2a41e18da5de8fb2c20bf5a5e52a41
Milla2000 <millajesso2000@gmail.com>
2023-11-08 21:35:14 +0300

    fixing overflow

--- js/script.js
+++ js/script.js
@@ -14,18 +14,19 @@ let navLinks = document.querySelectorAll("header nav a");
 //scroll section
 window.onscroll = () => {
     sections.forEach(sec => {
-        let top = window.scrollY;
-        // console.log(top)
-        let offset = sec.offsetTop - 100;
-        // console.log("offset" + sec.offsetTop)
-        let height = sec.offsetHeight;
+        let top = window.scrollY; //window.scrollY gives the current scroll position
+        console.log("top " + top)
+        let offset = sec.offsetTop - 100; //offsetTop gives the top position of the element in the window
+        console.log("offset " + offset)
+        let height = sec.offsetHeight; //offsetHeight gives the height of the element
+        console.log("height  " + height)
         let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height ) {
+        if (top >= offset && top < offset + height) {
             //active navbar links
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active'); 
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         }
     });
@@ -33,5 +34,11 @@ window.onscroll = () => {
     let header = document.querySelector('header')
     // console.log("milla")
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when user clicks navbar links 
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+
+
 
 }
