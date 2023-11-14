b6bed5514505ea820918bed3054b2337a9c067ec
Milla2000 <millajesso2000@gmail.com>
2023-11-14 14:29:42 +0300

    mobile style

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ window.onscroll = () => {
     menuIcon.classList.remove("bx-x");
     navbar.classList.remove("active");
 
-
-
+//    animation footer onscroll
+    let footer = document.querySelector("footer");
+    footer.classList.toggle("show-animate", this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
